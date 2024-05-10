9f3c54dbf83ec0f01695803befa949d02422fe15
michele laptop <micheledong2803@gmail.com>
2024-05-10 17:00:28 +0200

    secondo modo

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-const { createApp } = Vue;
+// const { createApp } = Vue;
 
+// createApp({
+//     data(){
+//         return {
+//             message: "Ciao"
+//         }
+//     }
+// }).mount("#app");
+
+const createApp = Vue.createApp;
+
+// createApp prende in ingresso un oggetto
 createApp({
+    // contenente per ora data(){} che è una funzione che restituisce un oggetto
     data(){
-        return {
+        return{
             message: "Ciao"
+        }
+    },
+    // contenente per ora methods che è una chiave che ha come valore un oggetto composta da funzioni
+    methods: {
+        ciao(){
+            console.log("ciao");
         }
     }
 }).mount("#app");
